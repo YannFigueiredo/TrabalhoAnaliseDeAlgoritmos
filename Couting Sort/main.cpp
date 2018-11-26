ba47b7b051df234710fb89fcbd5bb69252f44daa
yannf <yannf@DESKTOP-P3QOM02>
2018-11-26 11:57:07 -0300

    Atualizado para rodar com índice a partir de 0

--- Couting Sort/main.cpp
+++ Couting Sort/main.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
 void couting_sort(int A[],int B[],int n,int k){
-  int i,j,C[k];
+  int i,j,C[k+1];
   for(i=0;i<=k;i++)
     C[i] = 0;
-  for(j=1;j<=n;j++)
+  for(j=0;j<n;j++)
     C[A[j]] = C[A[j]] + 1;
   for(i=1;i<=k;i++)
     C[i] = C[i] + C[i - 1];
-  for(j=n;j>=1;j--){
-    B[C[A[j]]] = A[j];
-    C[A[j]] = C[A[j]] -1;
+  for(j=n-1;j>=0;j--){
+    B[C[A[j]]-1] = A[j];
+    C[A[j]] = C[A[j]] - 1;
   }
-  for(i=1;i<=n;i++)
+  cout << B[5] << endl;
+  for(i=0;i<n;i++)
     cout << B[i] << " ";
 }
 int main() {
-  int A[5],B[5],n,k;
-  cout << "Informe a quantidade de elementos do vetor: ";
-  cin >> n;
-  cout << endl;
-  for(int i=1;i<=n;i++){
-    cout << "Informe o elemento " << i << " do vetor A: ";
-    cin >> A[i];
-    cout << endl;
-    B[i] = 0;
-  }
-  cout << "\nVetor antes da ordenação:\n";
-  for(int i=1;i<=n;i++)
+  int A[]={2,2,3,1,0,1},X[7],B[6],n,k;
+  //clock_t Ticks[2];
+  n = 6;
+  k = 3;
+  //for(int i=1;i<=n;i++)
+  //  A[i] = X[i-1];
+  cout << "Vetor antes da ordenacao:\n";
+  for(int i=0;i<n;i++)
     cout << A[i] << " ";
-  cout << "\n\nInforme o maior numero do vetor A: ";
-  cin >> k;
-  cout << "\nVetor depois da ordenação:\n";
+  cout << "\n\nVetor depois da ordenacao:\n";
+  //Ticks[0] = clock();
   couting_sort(A,B,n,k);
+  //Ticks[1] = clock();
+  //double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+  //printf("\n\nTempo gasto: %g ms.", Tempo);
+  //getchar();
 }
